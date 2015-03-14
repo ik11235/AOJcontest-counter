81b7d448d8279669fa27e52c387b5331ec2b6a67
ik11235 <ik11235@gmail.com>
2015-03-14 15:54:36 +0900

    スクリプト初版追加

---
+++ main.rb
@@ -0,0 +1,82 @@
+require 'rexml/document'
+require 'open-uri'
+
+contestid="RitsCamp15Day1"
+url="http://judge.u-aizu.ac.jp/onlinejudge/webservice/contest_status_log?id="+contestid
+
+teams = [
+    "CALPIS",
+    "yoposu",
+    "too_Long",
+    "nimoji",
+    "Yanatsu",
+    "Jorkers",
+    "SKN",
+    "mod_gzip",
+    "kog",
+    "Doubling",
+    "WorkshopWitches",
+    "AccountSheet",
+    "omu_sawa_dan",
+    "WJK",
+    "UraKuni",
+    "ODD",
+]
+
+p url
+xml = open(url)
+doc = REXML::Document.new(xml)
+#puts doc
+allsubmit={}
+onsitesubmit = {}
+allac={}
+onsiteac={}
+doc.elements.each('contest_status/status') do |element|
+  problem_id=element.elements['problem_id'].text
+  user_id=element.elements['user_id'].text
+
+  if allsubmit.key?(problem_id) then
+    allsubmit[problem_id]=allsubmit[problem_id]+1
+  else
+    allsubmit[problem_id]=1
+  end
+  if element.elements['status_code'].text =="4"
+    if allac.key?(problem_id) then
+      allac[problem_id]=allac[problem_id]+1
+    else
+      allac[problem_id]=1
+    end
+  end
+
+  if teams.find { |team| team == user_id}
+    if onsitesubmit.key?(problem_id) then
+      onsitesubmit[problem_id]=onsitesubmit[problem_id]+1
+    else
+      onsitesubmit[problem_id]=1
+    end
+
+    if element.elements['status_code'].text =="4"
+      if onsiteac.key?(problem_id) then
+        onsiteac[problem_id]=onsiteac[problem_id]+1
+      else
+        onsiteac[problem_id]=1
+      end
+    end
+  end
+
+end
+#AC/submit
+#onsite or all
+#p onsitesubmit.to_a.sort
+puts "ONsite:"
+onsitesubmit.to_a.sort.each do |prog|
+  puts prog[0]+ ": "+onsiteac[prog[0]].to_s+ "/"+prog[1].to_s
+end
+puts "ALL:"
+allsubmit.to_a.sort.each do |prog|
+  puts prog[0]+ ": "+allac[prog[0]].to_s+ "/"+prog[1].to_s
+end
+
+#p onsiteac.to_a.sort
+#p allsubmit.to_a.sort
+#p allac.to_a.sort
