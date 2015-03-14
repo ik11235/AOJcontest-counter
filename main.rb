f99942a3bb70c4dbc90eb48ee8ec9866cef94dbc
ik11235 <ik11235@gmail.com>
2015-03-14 16:14:35 +0900

    ハッシュ存在判定のif文を簡略化

--- main.rb
+++ main.rb
@@ -36,32 +36,21 @@ doc.elements.each('contest_status/status') do |element|
   problem_id=element.elements['problem_id'].text
   user_id=element.elements['user_id'].text
 
-  if allsubmit.key?(problem_id) then
-    allsubmit[problem_id]=allsubmit[problem_id]+1
-  else
-    allsubmit[problem_id]=1
-  end
+  allsubmit[problem_id]=0 if !allsubmit.key?(problem_id)
+  allsubmit[problem_id]=allsubmit[problem_id]+1
+
   if element.elements['status_code'].text =="4"
-    if allac.key?(problem_id) then
-      allac[problem_id]=allac[problem_id]+1
-    else
-      allac[problem_id]=1
-    end
+    allac[problem_id]=0 if !allac.key?(problem_id)
+    allac[problem_id]=allac[problem_id]+1
   end
 
   if teams.find { |team| team == user_id}
-    if onsitesubmit.key?(problem_id) then
-      onsitesubmit[problem_id]=onsitesubmit[problem_id]+1
-    else
-      onsitesubmit[problem_id]=1
-    end
+    onsitesubmit[problem_id]=0 if !onsitesubmit.key?(problem_id)
+    onsitesubmit[problem_id]=onsitesubmit[problem_id]+1
 
     if element.elements['status_code'].text =="4"
-      if onsiteac.key?(problem_id) then
-        onsiteac[problem_id]=onsiteac[problem_id]+1
-      else
-        onsiteac[problem_id]=1
-      end
+     onsiteac[problem_id]=0 if !onsiteac.key?(problem_id)
+     onsiteac[problem_id]=onsiteac[problem_id]+1
     end
   end
 
@@ -72,8 +61,9 @@ onsitesubmit.to_a.sort.each do |prog|
   onsiteac[prog[0]]=0 if !onsiteac.key?(prog[0])
   puts "#{prog[0]}: #{onsiteac[prog[0]]}/#{prog[1]}"
 end
+
 puts "ALL:"
 allsubmit.to_a.sort.each do |prog|
-    allsiteac[prog[0]]=0 if !allac.key?(prog[0])
-    puts "#{prog[0]}: #{allac[prog[0]]}/#{prog[1]}"
+  allsiteac[prog[0]]=0 if !allac.key?(prog[0])
+  puts "#{prog[0]}: #{allac[prog[0]]}/#{prog[1]}"
 end
